c749af986370023a0dcf68df8c3d92ae9ba8bb91
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-10-11 01:42:16 +0530

    created pages & router setup for multiple pages with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+import Home from "./pages/Home/Home";
+import Details from "./pages/Details/Details";
+import SearchResult from "./pages/SearchResult/SearchResult";
+import Explore from "./pages/Explore/Explore";
+import Error from "./pages/Error/Error";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,9 +24,17 @@ function App() {
     apiTesting();
   }, []);
   return (
-    <div className="App">
-      <h1>{url.total_pages}</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
