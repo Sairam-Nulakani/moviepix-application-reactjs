83b28e105f947cd6e1228305d02e23cf2ad6f5c1
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-10-12 11:49:37 +0530

    updated footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./style.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Movies are a powerful medium of storytelling, capable of captivating
+          audiences with their unique blend of visuals, sound, and narrative.
+          They have the ability to transport viewers to different worlds, evoke
+          emotions, and provoke thought. From classic masterpieces to
+          contemporary blockbusters, movies offer a diverse range of experiences
+          that continue to shape and reflect our culture and society.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const SwitchTabs = () => {
+  return <div>Switch Tabs</div>;
+};
+
+export default SwitchTabs;
